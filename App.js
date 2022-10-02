0643d029ad52abd39eddaaf3db8cc08242bfbf89
Tanachat <s6401012630051@email.kmutnb.ac.th>
2022-10-02 17:58:07 +0700

    App

--- App.js
+++ App.js
@@ -1,112 +1,48 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from 'react'
+ import {View, Text,Image, StyleSheet} from 'react-native'
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+ const App = () => {
+  return(
+    <View style={styles.container}>
+      <Image source={{uri : 'https://randomuser.me/api/portraits/men/1.jpg'}} 
+        style={styles.image}
+      />
+      <Text style={styles.textStatus}>คุณได้รับประทานมื้อเช้าแล้ว</Text>
+      <Text style={styles.text}>วันนี้ได้รับ</Text>
+      <Text style={styles.textKcal}>543 kcal</Text>
     </View>
-  );
-};
+  )
+ }
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+ const styles = StyleSheet.create({
+  container : {
+    flex : 1,  alignItems: 'center'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  textStatus :{
+    fontFamily: 'Inter',
+    fontWeight: 'bold',
+    fontSize: 20,
+    color: '#000000',
+    top: 140
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  text :{
+    fontFamily: 'Inter',
+    color: '#000000',
+    fontSize: 16,
+    top: 180
   },
-  highlight: {
-    fontWeight: '700',
+  textKcal :{
+    fontFamily: 'Inter',
+    color: '#000000',
+    fontSize: 20,
+    top: 200
   },
-});
+  image :{
+    top: 100,
+    width: 100,
+    height: 100,
+    borderRadius : 100/2
+  }
+ })
 
-export default App;
+ export default App;
--- index.js
+++ index.js
@@ -5,6 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Screenwidthheigth from './screen/Screenwidthheigth';
+import Loading from './screen/Loading';
 
-AppRegistry.registerComponent(appName, () => Screenwidthheigth);
+AppRegistry.registerComponent(appName, () => App);
