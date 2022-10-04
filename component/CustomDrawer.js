a996291d746f2a959882c4188e648a7d3a0fd633
Tanachat <s6401012630051@email.kmutnb.ac.th>
2022-10-04 18:15:55 +0700

    navbar update

--- component/CustomDrawer.js
+++ component/CustomDrawer.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { View,Text } from "react-native";
+import { View,Text, ImageBackground,Image } from "react-native";
 import { 
     DrawerContentScrollView, 
     DrawerItemList 
 } from "@react-navigation/drawer";
 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
 const CustomDrawer = (props) => {
     return(
-        <DrawerContentScrollView {...props} 
-        contentContainerStyle={{backgroundColor: 'blue'}}>
-            <DrawerItemList {...props}/>
+        <View style={{flex:1, backgroundColor: '#CBA2A2'}}>
+        <DrawerContentScrollView {...props}>
+            <ImageBackground style={{padding: 20, marginTop: 60, marginLeft: 30, marginBottom: 20, marginRight: 50, alignItems:'center',}}>
+                <Image source={require('../img/profile.png')} style={{height:60, width:60}}/>
+                <Text style={{fontSize: 16, fontFamily: 'NotoSansThai', alignItems: 'center', top: 10}}>ชื่อ นามสกุล</Text>
+            </ImageBackground>
+            <View style={{flex:1, paddingHorizontal: 40}}>
+                <DrawerItemList {...props}/>
+            </View>
         </DrawerContentScrollView>
-
+        </View>
     )
 }
 
--- navbar/AppStack.js
+++ navbar/AppStack.js
@@ -15,7 +15,11 @@ const Drawer = createDrawerNavigator();
 const AppStack = () => {
     return(
         <Drawer.Navigator drawerContent={props => <CustomDrawer {...props} />} screenOptions={{headerShown: false}}>
-            <Drawer.Screen name='Home' component={HomeScreen}/>
+            <Drawer.Screen name='Home' component={HomeScreen} options={{
+                drawerIcon: ({color}) => (
+                    <Ionicons name='home-outline' size={22} color={color}/>
+                )
+            }}/>
             <Drawer.Screen name='Profile' component={ProfileScreen}/>
             <Drawer.Screen name='Menu' component={MenuScreen}/>
             <Drawer.Screen name='Statistic' component={StatisticScreen}/>
