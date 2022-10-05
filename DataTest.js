54d4257a19f8101322778aeadd9dd42caea7a87b
MessPh <99652149+MessPh@users.noreply.github.com>
2022-10-05 14:19:32 +0700

    Connect Database Success

---
+++ DataTest.js
@@ -0,0 +1,10 @@
+
+const MongoClient = require("mongoose")
+const uri ="mongodb://localhost:27017"
+connect();
+async function connect () {
+    MongoClient.connect(uri,{
+        useNewUrlParser:true,
+    useUnifiedTopology:false
+    }).then (()=>{console.log("Connect Laew")}).catch ((er)=>{console.log(er)})
+}
