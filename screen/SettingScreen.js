fc102b3e24b4dea05558c86b3b46609648ef7c3a
Tanachat <s6401012630051@email.kmutnb.ac.th>
2022-10-08 02:10:53 +0700

    Setting

--- screen/SettingScreen.js
+++ screen/SettingScreen.js
@@ -1,18 +1,56 @@
 import React from 'react'
-import {View, Text,Image, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity} from "react-native";
+
 
 const SettingScreen = () => {
     return(
         <View style={styles.container}>
-            <Text>Setting Screen</Text>
+            <Text style={styles.text}>ตั้งค่า</Text>
+            <View style={[styles.box,{marginTop:60,}]}>
+                <Text style={styles.text}>ภาษา</Text>
+                <TouchableOpacity onPress={() => {}} style={[styles.button,{marginLeft:75}]}>
+                    <Text style={styles.text}>ไทย</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.box}>
+                <Text style={styles.text}>แจ้งเตือน</Text>
+                <TouchableOpacity onPress={() => {}} style={[styles.button,{marginLeft:50}]}>
+                    <Text style={styles.text}>เปิด</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.box}>
+                <Text style={styles.text}>ออกจากระบบ</Text>
+                <TouchableOpacity onPress={() => {}} style={[styles.button,{marginLeft:25}]}>
+                    <Text style={styles.text}>ตกลง</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container : {
-      flex : 1,  alignItems: 'center'
+      flex : 1,  
+      marginTop:50
     },
+    text:{
+        fontFamily:'NotoSansThai',
+        fontSize:16,
+        textAlign:'center',
+        color: 'black'
+    },
+    box:{
+        flexDirection:'row',
+        marginBottom:60,
+        marginHorizontal:100,
+    },
+    button:{
+        backgroundColor:'#F0E4E4',
+        width:120,
+        height:25,
+        justifyContent: 'center',
+        borderRadius:10
+    }
 })
 
 export default SettingScreen ;
