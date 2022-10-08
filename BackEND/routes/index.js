43a3153a090efc0090168d195992f1e7a7c90835
MessPh <99652149+MessPh@users.noreply.github.com>
2022-10-08 16:53:03 +0700

    Update Validating Data

--- BackEND/routes/index.js
+++ BackEND/routes/index.js
@@ -20,35 +20,64 @@ router.post('/', function(req, res, next) {
       });
 });
 // Register
-router.post('/register',[
-  check("email","กรุณาป้อนอีเมล").isEmail(),
-  check("password","กรุณาป้อนพาสเวิร์ด").not().isEmpty(),
-  check("realname","กรุณาป้อนชื่อจริง").not().isEmpty(),
-  check("surname","กรุณาป้อนนามสกุล").not().isEmpty()
-], function(req,res,next){
+// router.post('/register',[
+//   check("email","กรุณาป้อนอีเมล").isEmail(),
+//   check("password","กรุณาป้อนพาสเวิร์ด").not().isEmpty(),
+//   check("realname","กรุณาป้อนชื่อจริง").not().isEmpty(),
+//   check("surname","กรุณาป้อนนามสกุล").not().isEmpty()
+// ], function(req,res,next){
+//   const result = validationResult(req);
+//   var err=result.errors;
+//   //validation data
+//   if(!result.isEmpty()){
+//     //Return error to views
+//     res.render('register',{errors})
+//   }},
+// function(req, res, next) {
+//   userdb = new data({
+//     Email:req.body.email,
+//     Password:req.body.password,
+//     Realname:req.body.realname,
+//     Surname:req.body.surname,
+//     Gender:req.body.gender,
+//     Age:req.body.age,
+//     Height:req.body.height,
+//     Weight:req.body.weight
+//   })
+//   data.createDB(userdb,function(err){
+//     if(err) console.log(err);
+//     res.redirect("/")
+//   });
+// });
+router.post("/register",[
+    check("email","กรุณาป้อนอีเมล").isEmail(),
+    check("password","กรุณาป้อนพาสเวิร์ด").not().isEmpty(),
+    check("realname","กรุณาป้อนชื่อจริง").not().isEmpty(),
+    check("surname","กรุณาป้อนนามสกุล").not().isEmpty()
+],function(req,res,next){
   const result = validationResult(req);
-  var err=result.errors;
-  //validation data
+  var errors = result.errors;
   if(!result.isEmpty()){
-    //Return error to views
-    res.render('register',{errors})
-  }},
-function(req, res, next) {
-  userdb = new data({
-    Email:req.body.email,
-    Password:req.body.password,
-    Realname:req.body.realname,
-    Surname:req.body.surname,
-    Gender:req.body.gender,
-    Age:req.body.age,
-    Height:req.body.height,
-    Weight:req.body.weight
-  })
-  data.createDB(userdb,function(err){
-    if(err) console.log(err);
-    res.redirect("/")
-  });
+    res.render("register",{errors:errors
+    })
+  }else{
+    userdb = new data({
+           Email:req.body.email,
+           Password:req.body.password,
+           Realname:req.body.realname,
+           Surname:req.body.surname,
+           Gender:req.body.gender,
+           Age:req.body.age,
+           Height:req.body.height,
+           Weight:req.body.weight
+         })
+         data.createDB(userdb,function(err){
+          if(err) console.log(err);
+          res.redirect("/")
+         })
+  }
 });
+
 
 module.exports = router;
 
