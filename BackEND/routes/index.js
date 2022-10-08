bee7470abdb54acccf3cc880ea5965ca79f41bc3
MessPh <99652149+MessPh@users.noreply.github.com>
2022-10-08 16:37:46 +0700

    Validating Data

--- BackEND/routes/index.js
+++ BackEND/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const data = require('../model/data');
 var router = express.Router();
+const{check,validationResult} = require("express-validator");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   data.getAllData(function(err,alldata){
@@ -19,15 +20,28 @@ router.post('/', function(req, res, next) {
       });
 });
 // Register
-router.post('/register', function(req, res, next) {
+router.post('/register',[
+  check("email","กรุณาป้อนอีเมล").isEmail(),
+  check("password","กรุณาป้อนพาสเวิร์ด").not().isEmpty(),
+  check("realname","กรุณาป้อนชื่อจริง").not().isEmpty(),
+  check("surname","กรุณาป้อนนามสกุล").not().isEmpty()
+], function(req,res,next){
+  const result = validationResult(req);
+  var err=result.errors;
+  //validation data
+  if(!result.isEmpty()){
+    //Return error to views
+    res.render('register',{errors})
+  }},
+function(req, res, next) {
   userdb = new data({
     Email:req.body.email,
-    Password:req.body.password
-    Realname:req.body.realname
-    Surname:req.body.surname
-    Gender:req.body.gender
-    Age:req.body.age
-    Height:req.body.height
+    Password:req.body.password,
+    Realname:req.body.realname,
+    Surname:req.body.surname,
+    Gender:req.body.gender,
+    Age:req.body.age,
+    Height:req.body.height,
     Weight:req.body.weight
   })
   data.createDB(userdb,function(err){
