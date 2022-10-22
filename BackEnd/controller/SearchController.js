94c03a2268b677df38c9440d228694833764129e
MessPh <99652149+MessPh@users.noreply.github.com>
2022-10-22 13:32:40 +0700

    Search

--- BackEnd/controller/SearchController.js
+++ BackEnd/controller/SearchController.js
@@ -1,12 +1,12 @@
-// const food = require("../model/food")
+ const food = require("../model/food.json")
 
-// exports.search = (req,res)=>{
-//     const {foodname} = req.body
-//     food.find({foodname:{$regex:foodname}}).then(food=>{
-//         if(!food[0]){
-//             return res.status(200).json({result:"not found"})
-//         }else{
-//             return res.status(200).json(food)
-//         }
-//     })
-// }
+ exports.search = (req,res)=>{
+     const {foodname} = req.body
+     food.find({foodname:{$regex:foodname}}).then(food=>{
+         if(!food[0]){
+             return res.status(200).json({result:"not found"})
+         }else{
+             return res.status(200).json(food)
+         }
+     })
+ }
