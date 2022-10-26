a5d56ab7714b452140da3ef8b66e738957bfcd23
MessPh <99652149+MessPh@users.noreply.github.com>
2022-10-26 14:08:37 +0700

    Update Cal

--- BackEnd/controller/CalculateCalController.js
+++ BackEnd/controller/CalculateCalController.js
@@ -1,10 +1,11 @@
 const foodcal = require("../model/foodcal.json")
 
 exports.calCal = (req,res)=>{
-    const FoodArray = req.body
+    const IngredientArray = req.body.ing
+    const PortionArray = parseint(req.body.por)
     const result = 0
     for (let i =0 ;i<FoodArray.length;i = i+1){
-        result = result + parseint(foodcal.FoodArray[i])
+        result = result + parseint(foodcal.IngredientArray[i])*PortionArray[i]
     }
     return res.status(200).json({result:result})
 }
